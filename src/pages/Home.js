38e3a3bd717a42aa96f5bd4bd29db94d144973eb
kishan daaby <85932117+krishna95711@users.noreply.github.com>
2023-03-02 20:52:48 +0530

    second commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { Box } from '@mui/material';
 import HeroBanner from '../components/HeroBanner';
 
-const Home = () => {
+export default function Home() {
   return (
-    <>
-      <Box>
-        <HeroBanner />
-      </Box>
-      <></>
-    </>
+    <Box>
+      <HeroBanner />
+    </Box>
   );
-};
-export default Home;
+}
